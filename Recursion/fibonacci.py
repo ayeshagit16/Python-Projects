214dd4e7347545fb99c0a1c83f15f5b8f2007370
Ayesha <ayesha.blr16@gmail.com>
2026-08-26 18:03:30 +0530

    fibonacci examples using recursion

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -1,4 +1,22 @@
 '''Fibonacci Series'''
 
 def fibonacci_series(n):
-    pass
+    '''Find the nth number in the fibonacci series
+    '''
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_series(n-1) + fibonacci_series(n-2)
+
+
+def generate_fibonacci(count, index=0):
+    '''Generate count Fibonacci numbers recursively.'''
+    if index == count:
+        return []
+    return [fibonacci_series(index)] + generate_fibonacci(count, index + 1)
+
+print(fibonacci_series(7))
+numbers = generate_fibonacci(1)
+print(numbers)
