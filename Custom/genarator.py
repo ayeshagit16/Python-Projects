635d653b4155e3f83954559e4dc31711f160689f
Ayesha <ayesha.blr16@gmail.com>
2026-09-08 00:29:10 +0530

    generators

--- Custom/genarator.py
+++ Custom/genarator.py
@@ -3,3 +3,25 @@
    This makes it highly memory-efficient because it creates values on demand (lazy evaluation) rather than 
         storing them in memory all at once
 '''
+
+def fibonacci_gen(n):
+    """A custom generator that yields the first n Fibonacci numbers."""
+
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a+b
+
+
+fibo_gen = fibonacci_gen(10)
+
+for num in fibo_gen:
+    print(num)
+
+print(list(fibonacci_gen(5)))
+
+fib_gen = fibonacci_gen(3)
+print(next(fib_gen))
+print(next(fib_gen))
+print(next(fib_gen))
+# print(next(fib_gen))   raises error StopIteration
